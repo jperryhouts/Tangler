1c55639e67aeabcdc5fa0c7d68d89baa06df7144
Jonathan Perry-Houts <6118266+jperryhouts@users.noreply.github.com>
2021-06-18 12:16:11 -0700

    Begin refactoring repository

---
+++ tangler/__init__.py
@@ -0,0 +1,11 @@
+# from distutils.core import setup
+
+
+def readme():
+    """Import the README.md Markdown file and try to convert it to RST format."""
+    try:
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst')
+    except(IOError, ImportError):
+        with open('README.md') as readme_file:
+            return readme_file.read()
